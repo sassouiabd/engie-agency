822024c8713945def22c14c22ccecab6862ad1fb
AS <sassouiabd@gmail.com>
2021-04-23 17:56:11 +0200

    [FEAT] sign-in page

---
+++ src/containers/app/app.container.tsx
@@ -0,0 +1,7 @@
+import SignIn from "../../pages/sign-in/sign-in.page";
+
+const App = () => {
+  return <SignIn />;
+};
+
+export default App;
---
+++ src/pages/sign-in/sign-in.page.jsx
@@ -0,0 +1,90 @@
+
+import Copyright from "../../components/copyright/copyright.component";
+import packageJson from "../../../package.json";
+
+// Material UI
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+// Styles
+import signIn_S from "./sign-in.styles";
+
+export default function SignIn() {
+  const S = signIn_S();
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={S.paper}>
+        <Avatar className={S.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <form className={S.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={S.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href='#' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright website={packageJson.name} />
+      </Box>
+    </Container>
+  );
+}
